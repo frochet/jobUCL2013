144a5be8c6f3fe9027e807e7d0628035e6720817
Florentin Rochet <oversnals@hotmail.com>
2013-09-05 10:01:15 +0200

    restore to correct version

--- labs/netkit-lab_webserver/create_topology.py
+++ labs/netkit-lab_webserver/create_topology.py
@@ -1,63 +1,62 @@
 
-# This file is used to create topologies for the traceroute lab.
+# This file is used to create topologies for the webserver lab.
 #
 #
 
-PREFIX="2001:db8:"
 
 import os
 import sys
 import getopt
 
-def configure(topo):
-	    create_r1(topo)
-	    create_r2(topo)
-	    create_r3(topo)
-	    create_r4(topo)
+def configure(onHosts):
+    for host in onHosts:
+        if host == "r":
+	    create_router()
+	elif host == "client1":
+	    create_client1()
+	elif host == "client2":
+	    create_client2()
+	elif host == "server":
+	    create_webserver()
+	
 
-def create_r1(topo):
-    os.makedirs(r1)
-    f=open('r1.startup','w')
-    f.write('ifconfig eth0 up \nifconfig eth0 add '+ global PREFIX + 'c001::1/48\nifconfig eth1 up\nifconfig eth1 add '+ global PREFIX + 'c001::2/48\nsysctl -w net.ipv6.conf.all.forwarding=1\nroute -A inet6 add default gw '+ global PREFIX + 'c001::3\necho "'+ global PREFIX + 'c001::3 r2" >> /etc/hosts\necho "'+ global PREFIX + 'cafe::2 r4" >> /etc/hosts\necho "'+ global PREFIX + '5afe::2 r3" >> /etc/hosts')
+def create_router():
+    f=open('r.startup','w')
+    f.write('ifconfig eth0 up \nifconfig eth0 add 2001:db8:0b0:15:da:b055::1/96\nifconfig eth1 up\nifconfig eth1 add 2001:db8:be:600d::1/64 \nsysctl -w net.ipv6.conf.all.forwarding=1\ninsmod sch_htb\ntc qdisc del dev eth0\ntc qdisc add dev eth0 root handle 1: htb default 1 \ntc class add dev eth0 parent 1: classid 1:1 htb rate 1000kbit\n')
     f.close()
 
-def create_r2(topo):
-    os.makedirs(r2)
-    f=open('r2.startup','w')
-    f.write('ifconfig eth0 up \nifconfig eth0 add '+ global PREFIX + 'c001::3/48\nifconfig eth1 up\nifconfig eth1 add '+ global PREFIX + '5afe::1/48\nifconfig eth2 up\nifconfig eth2 add '+ global PREFIX + 'cafe::1/48\nsysctl -w net.ipv6.conf.all.forwarding=1\nroute -A inet6 add '+ global PREFIX + '5afe::/48 gw '+ global PREFIX + 'cafe::2\necho "'+ global PREFIX + 'c001::1 r1" >> /etc/hosts\necho "'+ global PREFIX + 'cafe::2 r4" >> /etc/hosts\necho "'+ global PREFIX + '5afe::2 r3" >> /etc/hosts')
+def create_webserver():
+    f=open('server.startup','w')
+    f.write('ifconfig eth0 up\nifconfig eth0 add 2001:db8:be:600d::2/64\n/etc/init.d/apache2 start\nroute -A inet6 add default gw 2001:DB8:be:600d::1\n')
+    #cree des fichiers de taille 1 Mo, 10 Mo, 100Mo sur le webserver tres
+    #rapidement
+    f.write('dd if=/dev/zero of=/var/www/1Mo.zero bs=1K count=1000\n')
+    f.write('dd if=/dev/zero of=/var/www/10Mo.zero bs=1K count=10000\n')
+    f.write('dd if=/dev/zero of=/var/www/100Mo.zero bs=1K count=100000\n')
     f.close()
 
-def create_r3(topo):
-    os.makedirs(r3)
-    f=open('r3.startup','w')
-    f.write('ifconfig eth0 up \nifconfig eth0 add '+ global PREFIX + '5afe::2/48\nsysctl -w net.ipv6.conf.all.forwarding=1\nroute -A inet6 add default gw '+ global PREFIX + '5afe::1\necho "'+ global PREFIX + 'c001::1 r1" >> /etc/hosts\necho "'+ global PREFIX + 'cafe::2 r4" >> /etc/hosts\necho "'+ global PREFIX + '5afe::1 r2" >> /etc/hosts')
+def create_client2():
+    f=open('client2.startup','w')
+    f.write('ifconfig eth0 up\nifconfig eth0 add 2001:db8:0b0:15:da:b055::3/96\nroute -A inet6 add default gw 2001:DB8:0b0:15:da:b055::1\n')
+    f.write('echo "2001:db8:be:600d::2 webserver" > /etc/hosts\n')
     f.close()
 
-def create_r4(topo):
-    os.makedirs(r4)
-    f=open('r4.startup','w')
-    f.write('ifconfig eth0 up \nifconfig eth0 add '+ global PREFIX + 'cafe::2/48\nifconfig eth1 up\nifconfig eth1 add '+ global PREFIX + 'c001::4/48\nsysctl -w net.ipv6.conf.all.forwarding=1\nroute -A inet6 add '+ global PREFIX + '5afe::/48 gw '+ global PREFIX + 'cafe::1\necho "'+ global PREFIX + 'c001::1 r1" >> /etc/hosts\necho "'+ global PREFIX + 'cafe::1 r2" >> /etc/hosts\necho "'+ global PREFIX + '5afe::2 r3" >> /etc/hosts')
+def create_client1():
+    f=open('client1.startup','w')
+    f.write('ifconfig eth0 up\nifconfig eth0 add 2001:db8:0b0:15:da:b055::2/96 \nroute -A inet6 add default gw 2001:DB8:0b0:15:da:b055::1\n')
+    f.write('echo "2001:db8:be:600d::2 webserver" > /etc/hosts\n')
     f.close()
 
-def create_conf(lab):
+def create_conf():
     f=open('lab.conf','w')
-    f.write('LAB_DESCRIPTION="A lab where routers have bad route table. Use traceroute6 to correct them."\n LAB_VERSION=1\n LAB_AUTHOR="O. Bonaventure, J. Vellemans, F. Rochet"')
-    if (lab<=19):
-        f.write('r1[0]=A\nr1[1]=A\nr1[M]=64\nr2[0]=A\nr2[1]=C\nr2[2]=B\nr2[M]=64\nr3[0]=C\nr3[M]=64\nr4[0]=B\nr4[1]=A\nr4[M]=64')
-
-    if (lab <=29):
-        f.write('client1[0]=A\nClient2[0]=A\nr[0]=A\nr[1]=B\nserver[0]=B')
+    f.write('LAB_DESCRIPTION="A lab showing problems that can occurs when using tcp protocol to download files on a webserver"\n LAB_VERSION=1\n LAB_AUTHOR="O. Bonaventure, J. Vellemans, F. Rochet"')
+    f.write('client1[0]=A\nClient2[0]=A\nr[0]=A\nr[1]=B\nserver[0]=B')
     f.close()
 
 #main function
 def main(argv):
-    if argv[0] == "-A1" :
-        create_conf(1)
-        configure(1)
-           
-     
-    elif argv[0] == "-B1":
-        create_conf(2)
+    create_conf()
+    configure(["r", "server", "client1", "client2"])
     
 if __name__=="__main__":
     main(sys.argv[1:])
