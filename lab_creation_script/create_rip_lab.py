51e9434409ce642f8d3c9b85419878963435f0fc
Justin Vellemans <justin.vellemans@gmail.com>
2013-09-13 10:25:26 +0200

    espace

--- lab_creation_script/create_rip_lab.py
+++ lab_creation_script/create_rip_lab.py
@@ -5,39 +5,39 @@ from create_lab import Create_lab
 
 class Create_rip_lab(Create_lab):
 
-    def __init__(self,pathToGraph,pathToDir):
-        Create_lab.__init__(self,pathToGraph)
-        for node in self.graph.nodes():
-            r = Router(node)
-            self.netkit_components += [r]
-        self.set_interface_and_zone()
-        self.set_data_from_edges()
+  def __init__(self,pathToGraph,pathToDir):
+    Create_lab.__init__(self,pathToGraph)
+    for node in self.graph.nodes():
+      r = Router(node)
+      self.netkit_components += [r]
+    self.set_interface_and_zone()
+    self.set_data_from_edges()
 	self.give_ipv6("2001:db8:")
-        for router in self.netkit_components:
-            router.create_dir(pathToDir)
-            router.create_startup(pathToDir)
-            router.fill_startup_file(pathToDir)
-            router.create_ripng_dir(pathToDir)
-            
-        self.create_conf(pathToDir)
+    for router in self.netkit_components:
+      router.create_dir(pathToDir)
+      router.create_startup(pathToDir)
+      router.fill_startup_file(pathToDir)
+      router.create_ripng_dir(pathToDir)
+      
+    self.create_conf(pathToDir)
 def usage():
-    super(Create_rip_lab, self).usage()
-    print "python create_rip_lab -f [pathToDotFile] [pathToNetkitDirectory]"
-    
+  super(Create_rip_lab, self).usage()
+  print "python create_rip_lab -f [pathToDotFile] [pathToNetkitDirectory]"
+  
 def main(argv):
-    if len(argv)==0:
-        usage()
+  if len(argv)==0:
+    usage()
+  else:
+    if argv[0] == "-d":
+      pass # write default labs file
+    elif argv[0] =="-f":
+      lab = Create_rip_lab(argv[1], argv[2])
+    elif argv[0] == "-h":
+      usage()
     else:
-        if argv[0] == "-d":
-            pass # write default labs file
-        elif argv[0] =="-f":
-            lab = Create_rip_lab(argv[1], argv[2])
-        elif argv[0] == "-h":
-            usage()
-        else:
-            usage()
-            
+      usage()
+      
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+  main(sys.argv[1:])
