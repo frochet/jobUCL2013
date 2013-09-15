041c51bc7ea277eee92440bf9021b32631373a7e
Florentin Rochet <oversnals@hotmail.com>
2013-09-15 11:06:43 +0200

    pc and static lab coded

--- lab_creation_script/create_static_lab.py
+++ lab_creation_script/create_static_lab.py
@@ -7,15 +7,22 @@ class Create_static_lab(Create_lab):
     def __init__(self,pathToGraph,pathToDir):
         Create_lab.__init__(self,pathToGraph)
         for node in self.graph.nodes():
+	  if node[0] == 'r' or node[0] == 'R':
             r = Router(node)
             self.netkit_components += [r]
+	  elif node[0] == 'p' or node[0] == 'P':
+	    p = Pc(node)
+	    self.netkit_components += [p]
+	  else:
+	    print "malmorfed dot file, elem should be routers and/or pc in this lab. Please start the name of the components by r[...] for router and by p[...] for pc"
+	    sys.exit()
         self.set_interface_and_zone()
         self.set_data_from_edges()
         self.give_ipv6("2001:db8:")
-        for router in self.netkit_components:
-            router.create_dir(pathToDir)
-            router.create_startup(pathToDir)
-            router.fill_startup_file(pathToDir)
+        for component in self.netkit_components:
+            component.create_dir(pathToDir)
+            component.create_startup(pathToDir)
+            component.fill_startup_file(pathToDir)
 
         self.create_conf(pathToDir)
 def usage():
